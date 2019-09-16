c9f8d2f91fda90de376b02bbc2a468da3a7b6698
Maminov Artem <artem_maminov@mail.ru>
2019-09-16 16:55:02 +0300

    Function for plotting is moved to separated module

--- 2rpr_interval.py
+++ 2rpr_interval.py
@@ -86,46 +86,7 @@ Gright_lamb = sym.lambdify([u1, u2, v1, v2, v1mid, v2mid], Gright)
 
 Gright_lamb_no_Kr = sym.lambdify([u1, u2, v1, v2, v1mid, v2mid], G)
 
-def draw_and_compute(l1_l, l2_l, l1_h, l2_h, d, l1, l2):# процедура отрисовки 
-    Ax=[]
-    Ay=[]
-    if (l1_l<l1_h) and (l2_l<l2_h):
-        l1_l+=0.00000000000001
-        l2_l+=0.00000000000001
-        fig, ax = plt.subplots(figsize=(8, 8))
-        x_min, y_min, x_max, y_max=l1-1, l1-1, l2+1, l2+1
-        ax.set_xlim([x_min, x_max])
-        ax.set_ylim([y_min, y_max])
-        a = sg.Point(0,0).buffer(l1_l)# задаём оркужности через shapely, чтобы построить пересечение колец и найти площадь пересечения
-        b = sg.Point(0,0).buffer(l1_h)
-        c = sg.Point(d,0).buffer(l2_l)
-        e = sg.Point(d,0).buffer(l2_h)
-        ab=b.difference(a)# строим кольца из большой и малой окружности
-        cd=e.difference(c)
-        middle = ab.intersection(cd)
-        circle1 = Circle((0, 0), radius=l1_l, fill=False, color='r')# задаём окружности через matplotlib
-        circle2 = Circle((0, 0), radius=l1_h, fill=False, color='r')
-        circle3 = Circle((d, 0), radius=l2_l, fill=False, color='b')
-        circle4 = Circle((d, 0), radius=l2_h, fill=False, color='b')
-        rect1=Rectangle([l1, l1], abs(l1) + abs(l2), abs(l1) + abs(l2), fill=False, color='g', linewidth=2.0)
-        ax.add_patch(descartes.PolygonPatch(middle, fc='b', ec='k', alpha=0.2))# отрисовываем       
-        ax.add_patch(circle1)
-        ax.add_patch(circle2)
-        ax.add_patch(circle3)
-        ax.add_patch(circle4)
-        ax.add_patch(rect1)
-       # ax.grid()
-        ax.axes.set_aspect('equal')
-        for i in range(len(area_points_X_l)):
-            rect1=Rectangle([area_points_X_l[i], area_points_Y_l[i]],area_points_X_r[i] - area_points_X_l[i], 
-                            area_points_Y_r[i] - area_points_Y_l[i], fill=True, fc='red', color='g', linewidth=1.0, alpha=0.5)
-            ax.add_patch(rect1)
-        for i in range(len(border_points_X_l)):
-            rect2=Rectangle([border_points_X_l[i], border_points_Y_l[i]],border_points_X_r[i] - border_points_X_l[i], 
-                            border_points_Y_r[i] - border_points_Y_l[i], fill=True, fc='black', color='yellow', linewidth=1.0, alpha=0.5)
-            ax.add_patch(rect2)
-    else:
-        print('Неверные данные')
+
 
 def check_Box(u1, u2):#базовый алгоритм с исходными L1 и L2
   v1 = ival.Interval([L1, L2])
