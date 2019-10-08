df074267398af8223dc88ea020686e9ec822ea94
mposypkin <mposypkin@gmail.com>
2019-10-08 17:56:54 +0300

    Kravchik operator stuff

--- 2rpr_interval.py
+++ 2rpr_interval.py
@@ -28,8 +28,19 @@ def classical_krav_eval(u1, u2, l1, l2, d, p=10):
         v1mid = v1.mid()
         v2mid = v2.mid()
         v_krav = krav_transform(u1, u2, v1, v2, v1mid, v2mid, d)  # Calculate Kravchik evaluation for u1, u2
+        print("\nClassic krav:")
+        print("v1 = ", v1)
+        print("v2 = ", v2)
+        print("krav = ", v_krav)
+        print("Boundary krav:")
+        rv = boundary_krav_check(u1, u2, v1, v2, l1, l2, d)
+        if rv:
+            print("rv = ", rv)
         if (v_krav[0][0].isIn(v1)) and (v_krav[1][0].isIn(v2)):  # Compare Kravchik evaluation with v
             return 'inside'  # if it is inside previous interval, then it's inside the workspace area
+        elif boundary_krav_check(u1, u2, v1, v2, l1, l2, d):
+            print("Ura!")
+            return 'inside'
         if k == p - 1:
             return 'border'  # if we achieve max of the iterations, then it's border
         try:
@@ -39,7 +50,7 @@ def classical_krav_eval(u1, u2, l1, l2, d, p=10):
             return 'outside'
 
 
-def boundary_krav_eval(u1, u2, l1, l2, d, p=10):  # алгоритм с усиленной проверкой по 4 стороным
+def boundary_krav_check(u1, u2, v1, v2, l1, l2, d):  # алгоритм с усиленной проверкой по 4 стороным
     """
     Check the cell u with boundary Krawczyk operator if it is inside of the workspace area,
     outside or on the border
@@ -52,39 +63,32 @@ def boundary_krav_eval(u1, u2, l1, l2, d, p=10):  # алгоритм с усил
     :return: the string 'inside', 'outside' or 'border'
     """
     check = True
+    # return check
     # 4 bounds of the checking box v
-    v1_border = [ival.Interval([l1, l2]), ival.Interval([l2, l2]), ival.Interval([l1, l2]), ival.Interval([l1, l1])]
-    v2_border = [ival.Interval([l1, l1]), ival.Interval([l1, l2]), ival.Interval([l2, l2]), ival.Interval([l1, l2])]
+    # v1_border = [ival.Interval([l1, l2]), ival.Interval([l2, l2]), ival.Interval([l1, l2]), ival.Interval([l1, l1])]
+    # v2_border = [ival.Interval([l1, l1]), ival.Interval([l1, l2]), ival.Interval([l2, l2]), ival.Interval([l1, l2])]
+    v1_border = [ival.Interval([v1[0], v1[1]]), ival.Interval([v1[1], v1[1]]), ival.Interval([v1[0], v1[1]]), ival.Interval([v1[0], v1[0]])]
+    v2_border = [ival.Interval([v2[0], v2[0]]), ival.Interval([v2[0], v2[1]]), ival.Interval([v2[1], v2[1]]), ival.Interval([v2[0], v2[1]])]
     for s in range(4):  # Check all 4 boxes
-        v1 = v1_border[s]
-        v2 = v2_border[s]
-        for k in range(p):
-            v1mid = v1.mid()
-            v2mid = v2.mid()
-            v1_bor = ival.Interval([l1, l2])
-            v2_bor = ival.Interval([l1, l2])
-            v_krav = krav_transform(u1, u2, v1, v2, v1mid, v2mid, d) # Calculate Kravchik evaluation for u1, u2
-            if (v_krav[0][0].isIn(v1_bor)) and (v_krav[1][0].isIn(v2_bor)): # Compare Kravchik evaluation with v
-                check = True
-                if p == 3 and check:  # if it is inside all 4 boundaries, then it's inside the workspace area
-                    return 'inside'
-            else:
-                check = False
-            if k == p - 1:
-                return 'border'  # if we achieve max of the iterations, then it's border
-            try:
-                v1_bor.intersec(A[0][0])  # if our evalution not fully inside, then intersect it and repeat
-                v2_bor.intersec(A[1][0])
-                v1 = v1_bor
-                v2 = v2_bor
-            except:
-                return 'outside'  # if there is no intersection, then the cell is outside
+        v1mid = v1_border[s].mid()
+        v2mid = v2_border[s].mid()
+        v_krav = krav_transform(u1, u2, v1_border[s], v2_border[s], v1mid, v2mid, d) # Calculate Kravchik evaluation for u1, u2
+        print("v1 = ", v1_border[s])
+        print("v2 = ", v2_border[s])
+        print("v_krav = ", v_krav)
+        # print("v1_bor = ", v1_bor)
+        # print("v2_bor = ", v2_bor)
+
+        if not ((v_krav[0][0].isIn(v1_border[s])) and (v_krav[1][0].isIn(v2_border[s]))): # Compare Kravchik evaluation with v
+            check = False
+            break
+    return check
 
 
 d = 6
 L1 = 3  # Lower range of row
 L2 = 15  # Upper range of row
-N = 20  # The number of nodes on uniform grid
+N = 10  # The number of nodes on uniform grid
 l1 = -L2  # Left and lower border of uniform grid
 l2 = L2  # Right and upper border of uniform grid
 X1 = np.linspace(l1, l2, N)
@@ -94,6 +98,6 @@ k = 10  # Max number of iterations
 area_points = BoxPoints()
 border_points = BoxPoints()
 area_points, border_points = check_box(X, Y, N, L1, L2, d, classical_krav_eval, k)  # Calculate workspace area and border coordinates
-area_points_bound, border_points_bound = check_box(X, Y, N, L1, L2, d, boundary_krav_eval, k)  # Calculate workspace area and border coordinates
+# area_points_bound, border_points_bound = check_box(X, Y, N, L1, L2, d, boundary_krav_eval, k)  # Calculate workspace area and border coordinates
 plot_workspace(L1, L2, d, area_points, border_points)  # Plotting
-plot_workspace(L1, L2, d, area_points_bound, border_points_bound)
+# plot_workspace(L1, L2, d, area_points_bound, border_points_bound)
