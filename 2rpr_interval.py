[fix] fixed difference between classical method and exact calculation

2rpr_interval.py
@@ -4,7 +4,7 @@ import interval as ival
 from check_box import check_box
 from plot_workspace_area import plot_workspace
 from box_class import BoxPoints
-from kravchik_operator import get_krav_func, get_rec_func, get_rec_func_optim, zzf
+from kravchik_operator import get_krav_func, get_rec_func_optim
 
 krav_transform = get_krav_func()
 # rec_func = get_rec_func()
@@ -31,14 +31,15 @@ def classical_krav_eval(u1, u2, l1, l2, d, p=10):
         v1mid = v1.mid()
         v2mid = v2.mid()
         v_krav = krav_transform(u1, u2, v1, v2, v1mid, v2mid, d)  # Calculate Kravchik evaluation for u1, u2
-        #print("\nClassic krav:")
-        #print("v1 = ", v1)
-        #print("v2 = ", v2)
-        #print("u1 = ", u1)
-        #print("u2 = ", u2)
-        #print("v1mid = ", v1mid)
-        #print("v2mid", v2mid)
-        #print("krav = ", v_krav)
+        #if u1[0] == -4.090909090909092 and u1[1] == -1.363636363636365 and u2[0] == 6.818181818181817 and u2[1] == 9.545454545454543:
+            #print("\nClassic krav:", k)
+            #print("v1 = ", v1)
+            #print("v2 = ", v2)
+            #print("u1 = ", u1)
+            #print("u2 = ", u2)
+            #print("v1mid = ", v1mid)
+            #print("v2mid", v2mid)
+            #print("krav = ", v_krav)
         #print("Boundary krav:")
         if (v_krav[0][0].isIn(v1)) and (v_krav[1][0].isIn(v2)):  # Compare Kravchik evaluation with v
             return 'inside'  # if it is inside previous interval, then it's inside the workspace area
@@ -59,6 +60,7 @@ def classical_krav_eval(u1, u2, l1, l2, d, p=10):
         # except:                        # if there is no intersection, then the cell is outside
         #     return 'outside'
 
+
 def exact_eval(u1, u2, l1, l2, d, p=10):
     """
     Check the cell u with classical Krawczyk operator if it is inside of the workspace area,
@@ -76,9 +78,9 @@ def exact_eval(u1, u2, l1, l2, d, p=10):
     v2_init = ival.Interval([l1, l2])  # Interval form of Y-coordinate for box v
     v1 = ival.Interval([l1, l2])  # Interval form of X-coordinate for box v
     v2 = ival.Interval([l1, l2])  # Interval form of Y-coordinate for box v
-    v1mid = v1.mid()
-    v2mid = v2.mid()
     for k in range(p):
+        v1mid = v1.mid()
+        v2mid = v2.mid()
         v_exact = rec_func(u1, u2, v1, v2, v1mid, v2mid, d) # Calculate exact_inclusion for u1, u2
         #print("\nExact inclusion, k = ", k, ":")
         #print("v1 = ", v1)
@@ -144,14 +146,53 @@ def boundary_krav_check(u1, u2, v1, v2, d):  # алгоритм с усилен�
     return check
 
 
+def exact_krav_eval(u1, u2, l1, l2, d, p=10):
+    """
+    Check the cell u with classical Krawczyk operator if it is inside of the workspace area,
+    outside or on the border
+    :param u1: the X coordinates of cell u
+    :param u2: the Y coordinates of cell u
+    :param n: number of nodes of uniform grid
+    :param l1: the lowest range of 2-RPR rod
+    :param l2: the highest range of 2-RPR rod
+    :param d: the distance between rods
+    :param p: the max number of iterations
+    :return: the string 'inside', 'outside' or 'border'
+    """
+    v1_init = ival.Interval([l1, l2])  # Interval form of X-coordinate for box v
+    v2_init = ival.Interval([l1, l2])  # Interval form of Y-coordinate for box v
+    v1 = ival.Interval([l1, l2])  # Interval form of X-coordinate for box v
+    v2 = ival.Interval([l1, l2])  # Interval form of Y-coordinate for box v
+    for k in range(p):
+        v1mid = v1.mid()
+        v2mid = v2.mid()
+        v_exact = krav_func_opt(u1, u2, v1, v2, v1mid, v2mid, d) # Calculate exact_inclusion for u1, u2
+        #rec_func_alt(u1, u2, v1, v2, v1mid, v2mid, d)
+        #print("\nExact krav inclusion, k = ", k, ":")
+        #print("v1 = ", v1)
+        #print("v2 = ", v2)
+        #print("u1 = ", u1)
+        #print("u2 = ", u2)
+        #print("v1mid = ", v1mid)
+        #print("v2mid = ", v2mid)
+        #print("v_exact  = ", v_exact)
+        if (v_exact[0][0].isIn(v1)) and (v_exact[1][0].isIn(v2)):  # Compare Kravchik evaluation with v
+            return 'inside'  # if it is inside previous interval, then it's inside the workspace area
+        if k == p - 1:
+            return 'border'  # if we achieve max of the iterations, then it's border
+        if v1.isNoIntersec(v_exact[0][0]) or v2.isNoIntersec(v_exact[1][0]):
+            return 'outside'
+        else:
+            v1.intersec(v_exact[0][0])  # if our evalution not fully inside, then intersect it and repeat
+            v2.intersec(v_exact[1][0])
+
+
 d = 6
 L1 = 3  # Lower range of row
 L2 = 15  # Upper range of row
 N = 12  # The number of nodes on uniform grid
 l1 = -L2  # Left and lower border of uniform grid
 l2 = L2  # Right and upper border of uniform grid
-# l1 = 1
-# l2 = 3
 X1 = np.linspace(l1, l2, N)
 Y1 = np.linspace(l1, l2, N)
 X, Y = np.meshgrid(X1, Y1)  # Build X and Y of uniform grid
@@ -160,6 +201,5 @@ area_points = BoxPoints()
 border_points = BoxPoints()
 area_points, border_points = check_box(X, Y, N, L1, L2, d, exact_eval, k)  # Calculate workspace area and border coordinates
 area_points_def, border_points_def = check_box(X, Y, N, L1, L2, d, classical_krav_eval, k)  # Calculate workspace area and border coordinates
-# area_points_bound, border_points_bound = check_box(X, Y, N, L1, L2, d, boundary_krav_eval, k)  # Calculate workspace area and border coordinates
 plot_workspace(L1, L2, d, area_points, border_points)  # Plotting
 plot_workspace(L1, L2, d, area_points_def, border_points_def)